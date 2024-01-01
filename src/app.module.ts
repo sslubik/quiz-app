08ca2319f25d23924abb4e969505b85b1646b95c
Szymon Ślubik <szymonslubik@gmail.com>
2024-01-01 20:34:26 +0100

    Import GraphQLModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gpl')
+    }), 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
