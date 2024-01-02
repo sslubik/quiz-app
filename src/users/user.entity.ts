ba6ef91a99ec196c354c1b107944206957a68078
Szymon Ślubik <szymonslubik@gmail.com>
2024-01-02 19:15:38 +0100

    Add TypeORM decorators for User class and its attributes

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,28 +1,36 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 export enum UserRole {
-    Teacher = 'teacher',
-    Student = 'student'
+    TEACHER = 'teacher',
+    STUDENT = 'student'
 }
 
+@Entity()
 @ObjectType()
 export class User {
 
-    @Field(type => Int)
+    @PrimaryGeneratedColumn()
+    @Field(type => ID)
     user_id: number;
 
+    @Column()
     @Field()
     username: string;
 
+    @Column()
     @Field()
     email: string;
 
+    @Column()
     @Field()
     password: string;
     
-    @Field()
-    user_role: string;
+    @Column()
+    @Field(() => UserRole)
+    user_role: UserRole;
 
+    @Column()
     @Field()
     full_name: string;
 }
