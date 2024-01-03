f79802c543af6c1dd146bb1f6b9f44b3b167e835
sslubik <szymonslubik@gmail.com>
2024-01-03 03:38:36 +0100

    Create Docker related files and modified app.module to handle it

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,27 @@ import { UsersModule } from './users/users.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({}),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gpl')
     }), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'quiz_db',
+      synchronize: JSON.parse(process.env.POSTGRES_SYNCHRONIZE) || false,
+      autoLoadEntities: true,
+      entities: []
+    }),
     UsersModule
   ],
   controllers: [AppController],
