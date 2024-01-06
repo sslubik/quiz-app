7b101c6d80bedfd362194922f0ad9a2aa116997c
sslubik <szymonslubik@gmail.com>
2024-01-07 00:15:53 +0100

    Create service for ChoiceAnswers database entity

--- src/choice-answers/choice-answers.service.ts
+++ src/choice-answers/choice-answers.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
+import { CreateChoiceAnswerDto } from 'src/entities/dto/create.choice.answer.dto';
+import { Question } from 'src/entities/question.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ChoiceAnswersService {}
+export class ChoiceAnswersService {
+
+    constructor(
+        @InjectRepository(ChoiceAnswer)
+        private readonly choiceAnswersRepository: Repository<ChoiceAnswer>
+    ) {}
+
+    async findAll(): Promise<ChoiceAnswer[]> {
+        try {
+            return this.choiceAnswersRepository.find();
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    async findOne(id: number): Promise<ChoiceAnswer> {
+        try {
+            return this.choiceAnswersRepository.findOneBy({ id })
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    // async create(createChoiceAnswersDto: CreateChoiceAnswerDto[], question: Question): Promise<ChoiceAnswer[]> {
+    //     const array: ChoiceAnswer[] = [];
+    //     for (const createChoiceAnswerDto of createChoiceAnswersDto) {
+    //         const choiceAnswer = new ChoiceAnswer(createChoiceAnswerDto);
+    //         choiceAnswer.question = question;
+    //         array.push(choiceAnswer);
+    //     }
+    //     try {
+    //         return this.choiceAnswersRepository.save(array);
+    //     } catch(err) {
+    //         console.error(err);
+    //     }
+    // }
+}
