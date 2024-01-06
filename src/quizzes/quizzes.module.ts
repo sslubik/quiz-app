48945d6e742fd7b0b678355012090a31584c54d3
sslubik <szymonslubik@gmail.com>
2024-01-06 18:14:20 +0100

    Add providers for quizzes module

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quiz } from 'src/entities/quiz.entity';
+import { QuizzesResolver } from './quizzes.resolver';
+import { QuizzesService } from './quizzes.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Quiz])],
-    providers: []
+    providers: [QuizzesResolver, QuizzesService]
 })
 export class QuizzesModule {}
