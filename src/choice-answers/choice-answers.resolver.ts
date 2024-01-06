8d57c1eac4470e1dfde2ccf8b00737f4ce61811f
sslubik <szymonslubik@gmail.com>
2024-01-07 00:17:50 +0100

    Create GraphQL resolver for ChoiceAnswer database entities

--- src/choice-answers/choice-answers.resolver.ts
+++ src/choice-answers/choice-answers.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { ChoiceAnswersService } from './choice-answers.service';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
 
 @Resolver()
-export class ChoiceAnswersResolver {}
+export class ChoiceAnswersResolver {
+
+    constructor(private readonly choiceAnswersService: ChoiceAnswersService) {}
+
+    @Query(returns => [ChoiceAnswer])
+    async findAll(): Promise<ChoiceAnswer[]> {
+        return await this.choiceAnswersService.findAll();
+    }
+
+    @Query(returns => ChoiceAnswer)
+    async findOne(id: number): Promise<ChoiceAnswer> {
+        return await this.choiceAnswersService.findOne(id);
+    }
+}
