f7998133b9a11f4b12c14b3a30ad80363643491e
sslubik <szymonslubik@gmail.com>
2024-01-06 07:27:00 +0100

    Modify GraphQL Resolver to allow fetching one user as well as creating and deleting user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,14 +1,30 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from '../entities/user.entity';
+import { CreateUserDto } from 'src/entities/dto/create.user.dto';
 
 @Resolver(of => User)
 export class UsersResolver {
 
-    constructor(private usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
     @Query(retruns => [User])
-    users(): Promise<User[]> {
-        return this.usersService.findAll();
+    async findAllUsers(): Promise<User[]> {
+        return await this.usersService.findAll();
+    }
+
+    @Query(returns => User)
+    async findOneUser(@Args('id', { type: () => Int }) id: number) {
+        return await this.usersService.findOne(id);
+    }
+
+    @Mutation(returns => User)
+    async createUser(@Args('createUserDto') createUserDto: CreateUserDto) {
+        return await this.usersService.create(createUserDto);
+    }
+
+    @Mutation(returns => User, { nullable: true })
+    async removeUser(@Args('id', { type: () => Int }) id: number) {
+        return await this.usersService.remove(id);
     }
 }
