cd0ad2fcc92a1fa766eab4e245b0ce583978ef91
sslubik <szymonslubik@gmail.com>
2024-01-07 00:36:17 +0100

    Create module for SortingAnswer database entity

---
+++ src/sorting-answers/sorting-answers.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { SortingAnswersResolver } from './sorting-answers.resolver';
+import { SortingAnswersService } from './sorting-answers.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([SortingAnswer])],
+  providers: [SortingAnswersResolver, SortingAnswersService]
+})
+export class SortingAnswersModule {}
