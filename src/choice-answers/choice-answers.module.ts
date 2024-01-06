719ddf4d6c895493b27d598c407bfc261f390fd0
sslubik <szymonslubik@gmail.com>
2024-01-07 00:17:20 +0100

    Add ChoiceAnswer database entity to TypeOrmModule

--- src/choice-answers/choice-answers.module.ts
+++ src/choice-answers/choice-answers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChoiceAnswersService } from './choice-answers.service';
 import { ChoiceAnswersResolver } from './choice-answers.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChoiceAnswer])],
   providers: [ChoiceAnswersService, ChoiceAnswersResolver]
 })
 export class ChoiceAnswersModule {}
