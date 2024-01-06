99b4331c9f8e7f0fe1158c1ae0ab5f613da87d77
sslubik <szymonslubik@gmail.com>
2024-01-06 07:24:59 +0100

    Remove users module dependency on HTTP controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,9 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, UsersResolver],
-  controllers: [UsersController]
+  providers: [UsersService, UsersResolver]
 })
 export class UsersModule {}
