de8ebe5473a6d0bbed4c6d139aa85daf70b678d4
sslubik <szymonslubik@gmail.com>
2024-01-07 00:55:27 +0100

    Create TextAnswers module

---
+++ src/text-answers/text-answers.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TextAnswersResolver } from './text-answers.resolver';
+import { TextAnswersService } from './text-answers.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TextAnswer } from 'src/entities/text.answer.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TextAnswer])],
+  providers: [TextAnswersResolver, TextAnswersService]
+})
+export class TextAnswersModule {}
