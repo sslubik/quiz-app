ec7c7ade526835dd3806e2d13822b1b5220a76cd
sslubik <szymonslubik@gmail.com>
2024-01-07 23:04:14 +0100

    Add method for creating a transaction in QuizzesService class

--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
 import { CreateQuizDto } from 'src/entities/dto/create.quiz.dto';
+import { Question, QuestionTypeEnum } from 'src/entities/question.entity';
 import { Quiz } from 'src/entities/quiz.entity';
-import { Repository } from 'typeorm';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+import { TextAnswer } from 'src/entities/text.answer.entity';
+import { Repository} from 'typeorm';
 
 @Injectable()
 export class QuizzesService {
 
     constructor(
         @InjectRepository(Quiz)
-        private readonly quizzesRepository: Repository<Quiz>
+        private readonly quizzesRepository: Repository<Quiz>,
+        @InjectRepository(Question)
+        private readonly questionRepository: Repository<Question>,
+        @InjectRepository(ChoiceAnswer)
+        private readonly choiceAnswersRepository: Repository<ChoiceAnswer>,
+        @InjectRepository(SortingAnswer)
+        private readonly sortingAnswersRepository: Repository<SortingAnswer>,
+        @InjectRepository(TextAnswer)
+        private readonly textAnswersRepository: Repository<TextAnswer>
     ) {}
 
     async findAll(): Promise<Quiz[]> {
@@ -30,14 +42,60 @@ export class QuizzesService {
 
     async findByUserId(user_id: number): Promise<Quiz[]> {
       try {
-        return this.quizzesRepository.findBy({ user_id: user_id });
+        return this.quizzesRepository.findBy({ user_id });
       } catch(err) {
         console.error(err);
       }
     }
 
-    async createQuiz(createQuiz: CreateQuizDto) {
-        const { questions, ...newQuiz } = createQuiz;
-        const quiz = new Quiz(newQuiz);
+    async createQuiz(createQuizDto: CreateQuizDto): Promise<Quiz> { 
+      try {
+        return await this.quizzesRepository.manager.transaction(async quizManager => {
+          const { questionsDto, ...newQuiz } = createQuizDto;
+          const quiz = new Quiz(newQuiz);
+
+          const savedQuiz = await this.quizzesRepository.save(quiz);
+
+          for (const questionDto of questionsDto) {
+            const { choiceAnswersDto,
+                    sortingAnswersDto, 
+                    textAnswersDto, 
+                    ...newQuestion } = questionDto;
+            const question = new Question(newQuestion);
+            question.quiz = savedQuiz;
+
+            const savedQuestion = await this.questionRepository.save(question);
+            
+            switch (questionDto.question_type) {
+              default:
+                console.error('Unknown question_type in Question entity: Unable to save the answers to the database!');
+              case QuestionTypeEnum.CLOSED_ENDED:
+                for (const choiceAnswerDto of choiceAnswersDto) {
+                  const choiceAnswer = new ChoiceAnswer(choiceAnswerDto);
+                  choiceAnswer.question = savedQuestion;
+
+                  this.choiceAnswersRepository.save(choiceAnswer);
+                }
+              case QuestionTypeEnum.SORTING:
+                for (const sortingAnswerDto of sortingAnswersDto) {
+                  const sortingAnswer = new SortingAnswer(sortingAnswerDto);
+                  sortingAnswer.question = question;
+
+                  this.sortingAnswersRepository.save(sortingAnswer);
+                }
+              case QuestionTypeEnum.OPEN_ENDED:
+                for (const textAnswerDto of textAnswersDto) {
+                  const textAnswer = new TextAnswer(textAnswerDto);
+                  textAnswer.question = question;
+
+                  this.textAnswersRepository.save(textAnswer);
+                }
+            }
+          }
+          return savedQuiz;
+        });
+      } catch (err) {
+        console.error(err);
+      }
     }
 }
