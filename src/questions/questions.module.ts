482dd138851eee2ecdf28ebd25dbbb5790b6a325
sslubik <szymonslubik@gmail.com>
2024-01-07 23:15:16 +0100

    Modify QuestionsModule by adding QuestionServie to providers and exports

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Question } from 'src/entities/question.entity';
+import { QuestionsService } from './questions.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Question])]
+    imports: [TypeOrmModule.forFeature([Question])],
+    providers: [QuestionsService]
 })
 export class QuestionsModule {}
