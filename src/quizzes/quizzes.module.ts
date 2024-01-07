36aa8041d6b89525eb2683ddc1be93c84802af2c
sslubik <szymonslubik@gmail.com>
2024-01-07 23:10:20 +0100

    Import all modules requred by quizzes module

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quiz } from 'src/entities/quiz.entity';
 import { QuizzesResolver } from './quizzes.resolver';
 import { QuizzesService } from './quizzes.service';
+import { QuestionsModule } from 'src/questions/questions.module';
+import { ChoiceAnswersModule } from 'src/choice-answers/choice-answers.module';
+import { SortingAnswersModule } from 'src/sorting-answers/sorting-answers.module';
+import { TextAnswersModule } from 'src/text-answers/text-answers.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Quiz])],
+    imports: [
+        TypeOrmModule.forFeature([Quiz]), 
+        QuestionsModule, 
+        ChoiceAnswersModule,
+        SortingAnswersModule,
+        TextAnswersModule
+    ],
     providers: [QuizzesResolver, QuizzesService],
     exports: [QuizzesService]
 })
