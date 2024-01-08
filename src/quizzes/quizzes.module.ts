2a1b0b0caa4ca95de7680003326f6efe2df9f935
sslubik <szymonslubik@gmail.com>
2024-01-08 09:07:54 +0100

    Remove unnecessary imports in QuizzesModule

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -3,21 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quiz } from 'src/entities/quiz.entity';
 import { QuizzesResolver } from './quizzes.resolver';
 import { QuizzesService } from './quizzes.service';
-import { Question } from 'src/entities/question.entity';
-import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
-import { SortingAnswer } from 'src/entities/sorting.answer.entity';
-import { TextAnswer } from 'src/entities/text.answer.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([
-            Quiz,
-            Question,
-            ChoiceAnswer,
-            SortingAnswer,
-            TextAnswer
-        ]), 
-    ],
+    imports: [TypeOrmModule.forFeature([Quiz])],
     providers: [QuizzesResolver, QuizzesService],
     exports: [QuizzesService]
 })
