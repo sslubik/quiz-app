a24fd89918d2d2fe1258b370c0b453f4caebb12b
sslubik <szymonslubik@gmail.com>
2024-01-08 03:11:12 +0100

    Import all database entities required for createQuiz() transaction

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -3,18 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quiz } from 'src/entities/quiz.entity';
 import { QuizzesResolver } from './quizzes.resolver';
 import { QuizzesService } from './quizzes.service';
-import { QuestionsModule } from 'src/questions/questions.module';
-import { ChoiceAnswersModule } from 'src/choice-answers/choice-answers.module';
-import { SortingAnswersModule } from 'src/sorting-answers/sorting-answers.module';
-import { TextAnswersModule } from 'src/text-answers/text-answers.module';
+import { Question } from 'src/entities/question.entity';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+import { TextAnswer } from 'src/entities/text.answer.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Quiz]), 
-        QuestionsModule, 
-        ChoiceAnswersModule,
-        SortingAnswersModule,
-        TextAnswersModule
+        TypeOrmModule.forFeature([
+            Quiz,
+            Question,
+            ChoiceAnswer,
+            SortingAnswer,
+            TextAnswer
+        ]), 
     ],
     providers: [QuizzesResolver, QuizzesService],
     exports: [QuizzesService]
