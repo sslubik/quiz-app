87423ceabe3dda1039b585d6de919ea6f0c21cd8
sslubik <szymonslubik@gmail.com>
2024-01-09 00:40:09 +0100

    Add AttemptsService and AttemptsResolver to AttemptsModule's providers

--- src/attempts/attempts.module.ts
+++ src/attempts/attempts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attempt } from 'src/entities/attempt.entity';
+import { AttemptsService } from './attempts.service';
+import { AttemptsResolver } from './attempts.resolver';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Attempt])],
-    providers: []
+    providers: [AttemptsService, AttemptsResolver]
 })
 export class AttemptsModule {}
