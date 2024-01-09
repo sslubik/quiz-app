d9e05fe3faa93f4df20e6b950a84d674ded45a4c
sslubik <szymonslubik@gmail.com>
2024-01-09 05:56:05 +0100

    Add method for finding AttemptQuestion[] by Attempt.id

--- src/attempts-questions/attempts-questions.service.ts
+++ src/attempts-questions/attempts-questions.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Attempt } from 'src/entities/attempt.entity';
 import { AttemptQuestion } from 'src/entities/attempt.question.entity';
 import { Repository } from 'typeorm';
 
@@ -10,4 +11,12 @@ export class AttemptsQuestionsService {
         @InjectRepository(AttemptQuestion)
         private readonly attemptsQuestionsRepository: Repository<AttemptQuestion>
     ) {}
+
+    async findByAttemptId(attempt_id: number): Promise<AttemptQuestion[]> {
+        try {
+            return this.attemptsQuestionsRepository.findBy({ attempt_id });
+        } catch(err) {
+            console.error(err);
+        }
+    }
 }
