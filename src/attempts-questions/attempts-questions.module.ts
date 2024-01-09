12ecc25a4b28ec64850d643f89ab1ac1e42dcfd2
sslubik <szymonslubik@gmail.com>
2024-01-09 05:52:32 +0100

    Export AttemptQuestionsService class

--- src/attempts-questions/attempts-questions.module.ts
+++ src/attempts-questions/attempts-questions.module.ts
@@ -3,9 +3,11 @@ import { AttemptsQuestionsResolver } from './attempts-questions.resolver';
 import { AttemptsQuestionsService } from './attempts-questions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttemptQuestion } from 'src/entities/attempt.question.entity';
+import { QuestionsModule } from 'src/questions/questions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AttemptQuestion])],
-  providers: [AttemptsQuestionsResolver, AttemptsQuestionsService]
+  imports: [TypeOrmModule.forFeature([AttemptQuestion]), QuestionsModule],
+  providers: [AttemptsQuestionsResolver, AttemptsQuestionsService],
+  exports: [AttemptsQuestionsService]
 })
 export class AttemptsQuestionsModule {}
