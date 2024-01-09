b51a8850979cceaa715cfeb3a0c4717cb98bb678
sslubik <szymonslubik@gmail.com>
2024-01-09 10:08:31 +0100

    Import TypeORM Repositories for answers for all question types

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Question } from 'src/entities/question.entity';
 import { QuestionsService } from './questions.service';
+import { QuestionsResolver } from './questions.resolver';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+import { TextAnswer } from 'src/entities/text.answer.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Question])],
-    providers: [QuestionsService],
+    imports: [
+        TypeOrmModule.forFeature([
+            Question,
+            ChoiceAnswer,
+            SortingAnswer,
+            TextAnswer
+        ])
+    ],
+    providers: [QuestionsService, QuestionsResolver],
     exports: [QuestionsService]
 })
 export class QuestionsModule {}
