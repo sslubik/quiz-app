c905980d00a063abe28d174eea96284f8aafd53b
sslubik <szymonslubik@gmail.com>
2024-01-09 05:50:30 +0100

    Add AttemptsQuestionsModule to AttemptsModule's imports

--- src/attempts/attempts.module.ts
+++ src/attempts/attempts.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attempt } from 'src/entities/attempt.entity';
 import { AttemptsService } from './attempts.service';
 import { AttemptsResolver } from './attempts.resolver';
+import { AttemptsQuestionsModule } from 'src/attempts-questions/attempts-questions.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Attempt])],
+    imports: [TypeOrmModule.forFeature([Attempt]), AttemptsQuestionsModule],
     providers: [AttemptsService, AttemptsResolver]
 })
 export class AttemptsModule {}
