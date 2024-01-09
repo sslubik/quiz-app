2c84db22daa50446fef76cad66d04a5c3fc47136
sslubik <szymonslubik@gmail.com>
2024-01-09 05:55:14 +0100

    Create @ResolverField for finding relatad question

--- src/attempts-questions/attempts-questions.resolver.ts
+++ src/attempts-questions/attempts-questions.resolver.ts
@@ -1,10 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { ResolveField, Resolver } from '@nestjs/graphql';
 import { AttemptsQuestionsService } from './attempts-questions.service';
+import { Question } from 'src/entities/question.entity';
+import { Param } from '@nestjs/common';
+import { AttemptQuestion } from 'src/entities/attempt.question.entity';
+import { QuestionsService } from 'src/questions/questions.service';
 
 @Resolver()
 export class AttemptsQuestionsResolver {
 
     constructor(
-        private readonly attemptsQuesitonsService: AttemptsQuestionsService
+        private readonly attemptsQuesitonsService: AttemptsQuestionsService,
+        private readonly questionsService: QuestionsService
     ) {}
+
+    @ResolveField('Questions', returns => Question)
+    async findQuestionsById(@Param() attemptQuestion: AttemptQuestion) {
+        return await this.questionsService.findByQuestionId(attemptQuestion.question_id);
+    }
 }
