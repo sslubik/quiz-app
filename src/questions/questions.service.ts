ac41ac6c4a142b8ba59a0935a08b14c0cd16e332
sslubik <szymonslubik@gmail.com>
2024-01-09 10:09:58 +0100

    Add method for finding question answers

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Question } from 'src/entities/question.entity';
+import { AnswersUnion } from 'src/entities/GraphQL unions/answers.union';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
+import { Question, QuestionTypeEnum } from 'src/entities/question.entity';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+import { TextAnswer } from 'src/entities/text.answer.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,12 +12,35 @@ export class QuestionsService {
 
     constructor(
         @InjectRepository(Question)
-        private readonly questionsRepository: Repository<Question>
+        private readonly questionsRepository: Repository<Question>,
+        @InjectRepository(ChoiceAnswer)
+        private readonly choiceAnswersRepository: Repository<ChoiceAnswer>,
+        @InjectRepository(SortingAnswer)
+        private readonly sortingAnswersRepository: Repository<SortingAnswer>,
+        @InjectRepository(TextAnswer)
+        private readonly textAnswersRepository: Repository<TextAnswer>
     ) {}
 
     async findByQuestionId(id: number): Promise<Question> {
         try {
             return this.questionsRepository.findOneBy({ id });
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    async findAnswers(question: Question): Promise<Array<typeof AnswersUnion>> {
+        try {
+            switch (question.question_type) {
+                case QuestionTypeEnum.CHOICE:
+                    return this.choiceAnswersRepository.findBy({ question });
+                case QuestionTypeEnum.SORTING:
+                    return this.sortingAnswersRepository.findBy({ question });
+                case QuestionTypeEnum.TEXT:
+                    return this.textAnswersRepository.findBy({ question });
+                default:
+                    throw new Error("Failed to find question answers: unknown question_type!");
+            }
         } catch(err) {
             console.error(err);
         }
