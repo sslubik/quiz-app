9ed8945e683715f4402a14415ac8b4dbb3d1cddd
sslubik <szymonslubik@gmail.com>
2024-01-11 00:53:15 +0100

    Import requred TypeORM dependencies

--- src/attempts/attempts.module.ts
+++ src/attempts/attempts.module.ts
@@ -4,9 +4,24 @@ import { Attempt } from 'src/entities/attempt.entity';
 import { AttemptsService } from './attempts.service';
 import { AttemptsResolver } from './attempts.resolver';
 import { AttemptsQuestionsModule } from 'src/attempts-questions/attempts-questions.module';
+import { AttemptQuestion } from 'src/entities/attempt.question.entity';
+import { Question } from 'src/entities/question.entity';
+import { ChoiceAnswer } from 'src/entities/choice.answer.entity';
+import { SortingAnswer } from 'src/entities/sorting.answer.entity';
+import { TextAnswer } from 'src/entities/text.answer.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Attempt]), AttemptsQuestionsModule],
+    imports: [
+        TypeOrmModule.forFeature([
+            Attempt,
+            AttemptQuestion,
+            Question,
+            ChoiceAnswer,
+            SortingAnswer,
+            TextAnswer
+        ]), 
+        AttemptsQuestionsModule,
+    ],
     providers: [AttemptsService, AttemptsResolver]
 })
 export class AttemptsModule {}
