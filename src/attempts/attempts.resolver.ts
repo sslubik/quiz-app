f4ce5258176014d6cced5f659b07c8abc7e8ccf4
sslubik <szymonslubik@gmail.com>
2024-01-11 00:55:18 +0100

    Add queries, mutations and resolvers required for solving quizzes

--- src/attempts/attempts.resolver.ts
+++ src/attempts/attempts.resolver.ts
@@ -1,9 +1,10 @@
-import { Resolver, Query, Mutation, Args, ResolveField, Parent } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ResolveField, Parent, Float } from '@nestjs/graphql';
 import { AttemptsService } from './attempts.service';
 import { Attempt } from 'src/entities/attempt.entity';
 import { CreateAttemptDto } from 'src/entities/dto/create.attempt.dto';
 import { AttemptQuestion } from 'src/entities/attempt.question.entity';
 import { AttemptsQuestionsService } from 'src/attempts-questions/attempts-questions.service';
+import { UpdateAttemptDto } from 'src/entities/dto/update.attempt.dto';
 
 @Resolver(of => Attempt)
 export class AttemptsResolver {
@@ -17,14 +18,34 @@ export class AttemptsResolver {
     async findAllAttempts() {
         return await this.attemptsService.findAll();
     }
+    
+    @Query(returns => Attempt)
+    async findAttemptById(@Args('ID') id: number) {
+        return await this.attemptsService.findOne(id);
+    }
+    
+    @ResolveField('attempt_questions', returns => [AttemptQuestion])
+    async findAttemptQuestions(@Parent() attempt: Attempt) {
+        return await this.attemptsQuestionsService.findByAttemptId(attempt.id);
+    }
 
+    @ResolveField('max_points', returns => Float)
+    async getMaxPoints(@Parent() attempt: Attempt) {
+        return await this.attemptsQuestionsService.getMaxPoints(attempt);
+    }
+
+    @ResolveField('score', returns => Float)
+    async getScore(@Parent() attempt: Attempt) {
+        return await this.attemptsQuestionsService.getScore(attempt);
+    }
+    
     @Mutation(returns => Attempt)
     async createAttempt(@Args('crateAttemptDto') createAttemptDto: CreateAttemptDto) {
         return await this.attemptsService.create(createAttemptDto);
     }
-
-    @ResolveField('attemptQuestions', returns => [AttemptQuestion])
-    async findAttemptQuestions(@Parent() attempt: Attempt) {
-        return await this.attemptsQuestionsService.findByAttemptId(attempt.id);
+    
+    @Mutation(returns => Attempt)
+    async updateAttempt(@Args('updateAttemptDto') updateAttemptDto: UpdateAttemptDto) {
+        return await this.attemptsService.updateAttempt(updateAttemptDto);
     }
 }
