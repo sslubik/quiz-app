9db6f5d3fb63edb9f5ed2b3d1feecdd3301a78ec
sslubik <szymonslubik@gmail.com>
2024-01-11 01:36:18 +0100

    Create methods for getting user's score and max_points

--- src/attempts-questions/attempts-questions.service.ts
+++ src/attempts-questions/attempts-questions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Attempt } from 'src/entities/attempt.entity';
 import { AttemptQuestion } from 'src/entities/attempt.question.entity';
+import { Question } from 'src/entities/question.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,12 +10,53 @@ export class AttemptsQuestionsService {
 
     constructor(
         @InjectRepository(AttemptQuestion)
-        private readonly attemptsQuestionsRepository: Repository<AttemptQuestion>
+        private readonly attemptsQuestionsRepository: Repository<AttemptQuestion>,
+        @InjectRepository(Question)
+        private readonly questionsRepository: Repository<Question>
     ) {}
 
     async findByAttemptId(attempt_id: number): Promise<AttemptQuestion[]> {
         try {
             return this.attemptsQuestionsRepository.findBy({ attempt_id });
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    async getMaxPoints(attempt: Attempt): Promise<number> {
+        try {
+            const attemptQuestions = await this.attemptsQuestionsRepository.findBy(
+                { attempt_id: attempt.id }
+            );
+            
+            let max_points = 0;
+
+            for (const attemptQuestion of attemptQuestions) {
+                const question = await this.questionsRepository.findOneBy(
+                    { attempts_questions: attemptQuestion }
+                );
+                max_points += question.max_points;
+            }
+ 
+            return max_points;
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    async getScore(attempt: Attempt): Promise<number> {
+        try {
+            const attemptQuestions = await this.attemptsQuestionsRepository.findBy(
+                { attempt_id: attempt.id }
+            );
+
+            let score = 0;
+            
+            for (const attemptQuestion of attemptQuestions) {
+                score += attemptQuestion.points_scored;
+            }
+
+            return score;
         } catch(err) {
             console.error(err);
         }
