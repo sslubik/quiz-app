26c734b16fcf8bb2e196348c8024bc36b2c06d49
sslubik <szymonslubik@gmail.com>
2024-01-11 01:35:15 +0100

    Import required TypeORM dependencies

--- src/attempts-questions/attempts-questions.module.ts
+++ src/attempts-questions/attempts-questions.module.ts
@@ -4,9 +4,16 @@ import { AttemptsQuestionsService } from './attempts-questions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttemptQuestion } from 'src/entities/attempt.question.entity';
 import { QuestionsModule } from 'src/questions/questions.module';
+import { Question } from 'src/entities/question.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AttemptQuestion]), QuestionsModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      AttemptQuestion,
+      Question
+    ]),
+     QuestionsModule
+  ],
   providers: [AttemptsQuestionsResolver, AttemptsQuestionsService],
   exports: [AttemptsQuestionsService]
 })
