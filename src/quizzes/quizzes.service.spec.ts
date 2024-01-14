b95d7b5218b94b7894a85bb181b6e385d1643ba4
sslubik <szymonslubik@gmail.com>
2024-01-14 12:07:30 +0100

    Add more tests for createQuiz method

--- src/quizzes/quizzes.service.spec.ts
+++ src/quizzes/quizzes.service.spec.ts
@@ -3,9 +3,9 @@ import { QuizzesService } from './quizzes.service';
 import { DataSource, Repository } from 'typeorm';
 import { Quiz } from '../entities/quiz.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { CreateQuizDto } from '../entities/dto/create.quiz.dto';
 import { createQuizDtoMock } from '../entities/mocks/createQuizDtoMock';
 import { deepCopy } from '../auxillery-functions/deepCopy';
+import { QuestionTypeEnum } from '../entities/question.entity';
 
 describe('QuizzesService', () => {
   let service: QuizzesService;
@@ -44,20 +44,101 @@ describe('QuizzesService', () => {
   });
 
   it('should create a quiz with questions and answers', async () => {
-    const createQuizDto = createQuizDtoMock;
-    const quiz = await service.createQuiz(createQuizDto);
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    const quiz = await service.createQuiz(createQuizDtoMockCopy);
     expect(quiz).toBeInstanceOf(Quiz);
   });
+
+  it('should throw an error about no questions in quiz', async () => {
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    createQuizDtoMockCopy.questionsDto = [];
+
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message).toContain('Quiz must have at least one question!');
+    }
+  })
 
   it('should throw an error about empty answers', async () => {
     const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
     createQuizDtoMockCopy.questionsDto.forEach(val => {
-      val.choiceAnswersDto = undefined;
+      val.sortingAnswersDto = [];
     })
 
-    // const createQuizDto: CreateQuizDto = createQuizDtoMock;
-    // console.log(createQuizDto.questionsDto);
-    // const quiz = await service.createQuiz(createQuizDto);
-    // expect(quiz).toBeUndefined();
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message).toContain('All questions must have at least one answer');
+    }
+  });
+
+  it('should throw an error about negative max points', async () => {
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    createQuizDtoMockCopy.questionsDto[1].max_points = -2;
+
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message).toContain('Max points for question cannot be lesser than 0');
+    }
+  });  
+  
+  it('should throw an error about no true answers', async () => {
+    // Setting every answer as false in the first choice question
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    createQuizDtoMockCopy.questionsDto.find(element => {
+      if (element.question_type === QuestionTypeEnum.CHOICE) {
+        element.choiceAnswersDto.forEach(val => val.is_correct = false);
+        return true;
+      }
+    })
+  
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message)
+        .toContain('Answers in choice type questions must have at least one true answer');
+    }
+  });
+
+  it('should throw an error about negative numbers in order', async () => {
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    createQuizDtoMockCopy.questionsDto.find(element => {
+      if (element.question_type === QuestionTypeEnum.SORTING) {
+        element.sortingAnswersDto[0].order = -1;
+        return true;
+      }
+    })
+
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message).toContain('Order in sorting answers must be greater than 0');
+    }
+  })
+
+  it('should throw an error about wrong increment in order', async () => {
+    const createQuizDtoMockCopy = deepCopy(createQuizDtoMock);
+    createQuizDtoMockCopy.questionsDto.find(element => {
+      if (element.question_type === QuestionTypeEnum.SORTING) {
+        element.sortingAnswersDto.sort((a, b) => b - a);
+        element.sortingAnswersDto[0] += 1;
+        return true;
+      }
+    })
+
+    try {
+      await service.createQuiz(createQuizDtoMockCopy);
+      fail('No error was thrown!');
+    } catch(err) {
+      expect(err.message).toContain('Incorrect sorting order');
+    }
   })
 });
+
